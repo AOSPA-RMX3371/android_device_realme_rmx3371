87b80d00aa2515be441c90cafb5334e8bcc1cdf0
Abhishek Shukla <91975183+abhis699@users.noreply.github.com>
2023-09-04 12:25:57 +0530

    RMX3371: Virtual A/B support

--- device.mk
+++ device.mk
@@ -23,6 +23,25 @@ $(call inherit-product, vendor/realme/RMX3371/RMX3371-vendor.mk)
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
 
+# A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+PRODUCT_PACKAGES += \
+    checkpoint_gc \
+    otapreopt_script
+
 # Audio
 TARGET_USES_BLUETOOTH_LE_AUDIO := true
 
